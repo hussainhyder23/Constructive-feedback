5d8cacbe4eeacfba5858297c18cd485810ecf119
VSVS Varma <saii.varma5@gmail.com>
2018-09-15 02:53:09 +0530

    routes to be added

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
-from flask import Flask
+from flask import Flask , render_templates
+from data import articles
 
 app=Flask(__name__)
 
+
+Feed=Feedback()
+
 @app.route('/')
 def index():
-	return 'Index'
+	return render_templates("index.html")
 
 
+@app.route('/app')
+def app():
+	return render_templates("app.html",feed=Feed)
 
 
-
-
+@app.route('/app/<string:id>')
+def app(id):
+	return render_templates("app.html",id=id)
 
 
 
@@ -25,3 +33,8 @@ def index():
 
 if __name__ == '__main__':
 	app.run(debug='true')
+
+
+# {%includes  %} for including files
+#use {% %} for logic
+#use {} for variables
