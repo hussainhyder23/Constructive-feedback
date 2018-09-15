dc669c6d1370c2cafb55316d2cb6d95fffefd08e
Prathyush Kruthiventi <prathyush.kruthiventi@gmail.com>
2018-09-15 13:30:51 +0530

    updated NLP and search

--- search.py
+++ search.py
@@ -8,7 +8,7 @@
 
 import nltk
 from nltk import word_tokenize
-import nltk
+#import nltk
 from nltk.corpus import stopwords
 import NLP
 
@@ -140,6 +140,116 @@ From a secret admirer (Mechanical -1) ''',
 '''esson learnt in CBIT.
 Most of us have too much of technical stuff, but we don't really speak up during interviews. Why? Because our lazy asses are accustomed to speaking Telugu. Communication skills in English are more important than any other frigging technical skills to get that dream job. I know most of you take a chill-pill feeling that you can do way better in life than your absolutely-rubbish English speaking professors in CBIT. Let me break it to you, you need to speak brilliantly (no matter what you speak) to ace your placements. Good written communication helps you weave shit stories in your answer sheet and eventually get an S Grade. When in doubt, confuse with extraordinary vocabulary. Try it, because it helps. So instead of wasting your time ogling at that hot chick near CSE block, do something worthwhile or you will never get to her. 
 No offense, but this is for your own benefit. ''',
+"""Because of one person P****ka the other 43 are facing a problem who genuinely worked hard and got selected. @Deloitte issue""",
+"""To all those who posted confessions on placement coordinators and have doubts regarding Deloitte, GROW A PAIR IN YOUR SACK, come to PO tomorrow in the afternoon instead being stealth and posting BS as a coward. All doubts shall be cleared.- From a placement coordinator who is working his ass off for FREE and no particular reason just for the sake of placements of his fellow mates. """,
+"""Guys if you have any issue with sign out clause, deal the issue with Placement Head. But DO NOT mail Deloitte it will get the whole campus in trouble. They WILL blacklist cbit for rest of the years. If anyone wants to beat the shit out of Placement Head or any CPC go ahead. We will turn their asses blue but DO NOT mail Deloitte.""",
+"""Everything is fine regarding discussion about Deloitte. If such a heat is raged,atleast CPC or any of the PCs could post some confession or atleast comment stating the reasons which made them to act like that so that everyone will be calm.But you are not doing that. 
+It clearly shows You did a mistake.You guys are just barking rubbing instead of that post the thing which actually made you to act like that,so that everyone will understand situation and all will get chilled up.
+Peace be upon!
+Jai chenki!""",
+"""Guys,PC’s don’t have any special rights or what so ever.They just follow the instructions given to them by the placement head.They specified the SIGN OUT clause because they were instructed to do so and now the people already placed in deloitte were allowed because they were again instructed to do so.Just stop blaming PC’s.If something is bothering you so much,you can directly speak to the placement head.""",
+"""For guys worrying about Deloitte placement:
+
+Don't worry that you didn't get into it. Just Google and identify how good the company is and check for better ones that visit us or otherwise try your luck off-campus.
+We are not in a PERFECT Society, so don't expect everything to be clean and clear. I can see the venting out frustration from past few posts which is completely understandable and not wrong.
+
+I won't say to go and discuss with Placement officer as nothing gonna happen with that. Better discuss with your classmates/friends and then PCs to come up with some solution. They are representating you and doing a voluntary job. Few take it as a position that gives them super powers. Ignore them. But most guys will be trying to get everyone placed, which is where the guys with backlogs get into process. Some backdoor things like requesting HR for considering or reconsidered candidates do happen even at top Institutes of India. So don't blame for that. If you need such help, ask for it and don't be jealous of someone getting such help
+
+For PCs, especially guys sending out confessions:
+
+You guys are just increasing the heat of discussion instead of concluding it somewhere. Mistakes do happen, accept it and move on. After all we are humans and things never go as we expected.""",
+"""Arey stupid guys if you have any problem then raise directly in the placement office there will be no use if you bark in the confessions page, but think once before if you want to give complaint to Deloitte bcoz the company doesn't have anything to do regarding this clause""",
+"""Bro!! I can understand that you are selected for Deloitte can Plz shut ur a*s now?. If can directly come and talk in Placement office then why the f*ck are these PC`s for?? and why the f*ck they did so much SHOW about that Signout clause?? and I even asked one of the PC the only reply he gave me was "NLN Sir asked us to allow and we can't do anything" and even if come to Placement office then I`ll get the same answer and I`ll be targeted for the entire placement season. So Stop ur fucking Bits of Advice now !!""",
+"""Confession page is Lit AF.....Reply pls Chief placement garu......expecting something from u...
+atleast on PCs""",
+"""i see no point in complaining to deloitte i can only see jealousy becoz if sign out clause is the prblm then u shoud have raised the complaint in placement cell so that they will be strict regarding the clause .what do deloitte have to do with it?...u rejected deloitte becoz u are thops but we are not we just need a job.dont put your finger in the past it wont help u anyway.it will only effect our juniors""",
+"""Ok Fine we completely Accept your point and if you dont have any clarity about Signout clause then why you people stressed so much on it?? you shouldn't do it. Anyways the other point is at first you guys said you`ll not allow students with backlogs then again you allowed some people saying 1 Backlog is allowed then how come one PC with 2 backlogs attempted the test?? and she even got an offer!! You are not supposed to misuse the power you have that is so wrong. what explanation will you give for this??""",
+"""I like your way of conveying message in calm and composed manner not letting anyone feelings get hurt.
+But still The PCs are totally irresponsible,if sign out class is not such a serious one then many would have attended Deloitte .Yeah some didn't attended because they wanted better option but still just think it is sign out that prevented many to attend.Now most of the people who got placed if they get better opportunities they will just leave Deloitte and their vacancies are just void.If they wouldn't have attended or atleast sign out is made perfect then atleast who are really in need of job would have secured one na.
+Ps:I am saying about who are in need of job,now dont just say that why didn't you made in Deloitte.Not everyone is equally talented in everything.""",
+"""Okay I see a lot of hate about the Deloitte situation. I was quiet for a long time but this is getting ridiculous. This is for the people who have decided to opt out of the Deloitte recruitment because of the signout clause. 
+Firstly, you guys have decided on your own that you do not want Deloitte and that you'd want to try for a better company. No one has forced you to not sit for the recruitment. Secondly, none of the people who have attended the recruitment knew that they'll be allowed to sit for other companies. They have made their choice and decided to risk their chances of getting placed at a better company. But now since they are being allowed to sit for other companies, they are. 
+Now do you think making a fuss about this is going to help anyone? Hasty decisions of sending mails to the HRs is not going to affect any of the PCs or the students who already got placed at Deloitte. It will just lead to the company blacklisting CBIT in the coming years which will affect the juniors a lot. Whatever done is done. When you were so sure about bagging another offer that you decided to opt out of Deloitte why not concentrate on that and get placed at a crazy company that you wont have to look back on Deloitte? I know that you guys are pissed. But give it a thought before taking such decisions. Peace.""",
+"""Funny how the fees increased this year and The Transcendent newsletter was colourless.""",
+"""20th Century or 21st? Anyways... He used you? Or you got used by him? Wasnt it ur choice to make him a friend of urs? Why a confession why not a direct slap on his face? Anyways Karma will F**k him eventually.""",
+"""Simple !
+Just fucking listen !
+Lot of you are filled with lust, so much that you are openly talking about a girl about how she looks and about her curves.
+I'm a guy and I'm a fucking feminist
+I don't care what y'all idiots think.
+When something like nirbhaya incident happens some of you might feel sad some of you might curse our government for not working perfectly 
+What about you bitches ? Are you normal ? You are a fucking horny asshole judging people by their looks 
+Don't you dare do that !
+What if your sister or a relative was judged inform of everyone so loud that they can hear?
+How would that feel?
+Bring a change you guys. If anyone around you talks in such a fuckall manner just slap them so hard that they will remember for their entire life.""",
+"""Hey Nath! HAHAHAHAHA I got into JMPC and you didn't xD How does it feel brother""",
+"""hat one handsome guy in my class..... I love his soo much chubby chubby.. He's amazing coder and from saudi.... He loves someone else so i couldn't get a chance ! Roll no 162 .... I love you!
+CSE3 3/4""",
+"""Cbit history Cbit like never before
+I wrote Advance supply which I have been already passed and got A Grade in the Main examination Cbit like never before """,
+"""Why are students not given receipt for the placement fee?
+is it happening without the knowledge of management ? 
+is the money going into pockets of NLN or the student coordinators ?
+why did our senior PCs party with the placement fee money ?""",
+"""Admin bhayya You are not posting Confessions related to placements ,even i have experienced this twice.Today someone pointed out so even I wanna also point this out.We are sharing facts and why do you fear to do post confession,Its because You are a puppet of NLN.
+Sorry if it hurts you,but tell the reason why arent u posting all the confessions and why are you filtering them
+
+Admin: Bro first point is just check properly if have submitted the confession or not?? and second I know we are Indians and we like to do what others do so u even belong to the same category so that is why u are pointing it out now if not then you could have pointed it out early, and I already told you in the previous confession that I even posted confessions against principal even though he is following this page and why will I fear for this now? and why the Fu*k I will be a puppet of someone?? Mind your words before you talk and if that's true then I wouldn't have posted even a single confession about PC`s or PO and I can understand that you don't follow this page. if I filter the confession then there won't be such a huge number on board. I know CBIT has illiterates but never expected an illiterate of this extent. Sorry if it hurts you """,
+"""Dear admin ,
+Why don’t you post confessions related to placement office .is it because you are a CPC and you are part of our shitty placement office .let the people know the scams of the placement office and be a right student and admin
+
+Admin: LOLOLOL!!! Me part of a placement office?? OMG!! I couldn't even believe this and moreover CPC ah?? I don't even know the difference between them and Did u ever followed this Page??!! For your kind info read the confession number #1877 and tell me whether it is related to PC`s and PO or not?? and when I even posted Confession About the principal(Do u know principal follows this page?) and other depts then why will I hide this??""",
+"""That confession was the most pointless shit I’ve ever seen. It could’ve been for anyone. It could’ve been for me from my fucking crush. But nobody would ever know becuase you thought “oh let’s tell everyone that I like this person but you know what would be fun not telling them who it is.” WHAT IS THE POINT. You stupid banana""",
+"""Day 6995 without sex:
+Wanked to my fee receipt because the fees looked hella thicc.""",
+"""The most Handsome boy in CBIT is Sai Krishna Rao G. The day I saw u I lost my mind .. u r the sweetest n coolest guy I have ever seen ...u r the best human in this world .. u r charming looks ... U r attitude makes me high ... Mr.frehser I'm hoping that u should read this ..I'll b waiting for u and your response ...
+
+This is to my love @Sai Krishna""",
+"""A wish for you on your birthday, whatever you ask may you receive, whatever you seek may you find, whatever you wish may it be fulfilled on your birthday and always. Happy birthday CHANHU 
+(Sai kiran) 
+I don’t know when it will pasted in this page ...but any way advance or belated returns bruh""",
+"""reply to 1882, process to become teaching staff at cbit is a open secret which no one talks about....""",
+"""Respected principal,AEC,
+CBIT.
+Sub:- Return my revalve fees as the true face of CBIT is revealed.
+Sir, i am just another frustuated but still proudly so called CBITian. Instead of hike in exam fees,condonation fees,tuition fees,university fees,bus fees,etc etc better you had increase standard and education level of CBIT, also got to know we have to even pay for graduation day, please stop playing with lifes of innocent students. CBIT is only good at collecting money by either this or that way from students. Among all faculty members i hardly found only 1 or 2 who are good at teaching, i am very glad to know what is the process to become teaching staff in CBIT and why the process to become student of CBIT is so over rated and disguise. CBIT should get a Guinness world record for conducting supply before the revalve results. There is evrything wrong with placements from pcs to number of selections even for this we have to pay placement fees. After all these i am still a proudly so called CBITian. And i am so much inspired from CBIT to make me learn how cunning,selfish,evil the world is.
+Thanking you,
+Yours sincerely,
+a CBITian.""",
+"""Hyder of 2/4 H2 . Why did u remove sayat. Don't let the negativity affect u. U are really very sweet and cute. I like u. Be happy. Lots of love.""",
+"""I love you :').... If i tell you in person i know everything will end.. Take care love... You will always be my sunshine """,
+"""The Communicando and Transcendent senior who wore a black shirt on Thursday, it was impossible to focus on the interview when you were taking it! You're devilishly handsome, your smile is satanic!""",
+"""Does CBIT management and principal has shame on their part and brain in their heads .will any one on the earth conduct graduation day so late and that too on a Friday with a fee with a venue not in the college.
+My dear management if you don’t change your behaviour and money minded nature the glory of CBIT will completely be gone.""",
+"""Hey you guys 
+I'm Dev Yashwanth a huge Linkin Park fan.
+I want all of you to see the posters I've stuck on the notice board near the canteen whenever you go to college the next time.
+Please see that and start a conversation about it.
+Let's take this to another level.
+Last year this day my hero Chester bennington passed away by suicide and a part of the world lost their hero.
+His music helped me and a lots through some of the toughest and darkest times which we can't publicly talk about. It's not fair that we healed from our troubles but he couldn't get out of his troubles or " demons to say. 
+I want everyone to start a conversation about love caring depression. Love others care for them, reach out to them.
+The change comes within , that change will then spread to the entire world.
+Please you guys. I've lost someone for depression and suicide and no one probably reached out to him. Please don't let this happen to anyone ever again. There will be no signs of depression.
+Take care you guys!""",
+"""To syed farhan of it-1 
+I wont hesitate to say anything but your soo cute, i know you are not mine or can ever be.. maybe but still seeing you smile makes my day. I never leave any opportunity to admire you be it anywhere canteen, bus bay. There’s no way i can describe my love for you u i think abt u every morning that i would get to see you.
+But dont have the guts to talk to you.. will do someday but
+Your secret admirer
+CBIT Confessions""",
+"""Hey you guys !
+This is Dev Yashwanth. 
+I'm a guitarist and a singer.
+Looking for musicians( guitarists , bassists , drummers ) to jam with. I play stuff like Guns N' Roses Linkin Park Soungarden Slash Michael Jackson....so on.
+Wanna jam to some rock & roll.
+Sick of the edm and pop shit popping out everywhere.
+So if anyone's interested leave a comment like hi or something I'll get in touch.
+Let's rip the roof off !
+Cheers!""",
+"""I was walking on the floor of c-block ,while i was walking I suddenly seen a boy who is cute and sweet like a laddu, I love his belly so much and when I researched about him i found that his name is Yogesh Luckshetty of CSE _1 third year and everybody calls him jumbo becoz he looks like laddu mithai""",
+"""Is it mandatory to be present at the convocation, if we have one ?""",
+
 
 ]
 
@@ -147,7 +257,6 @@ No offense, but this is for your own benefit. ''',
 # In[4]:
 
 
-filtered,profanity,polarity=NLP.filter(feedback)
 
 
 # In[5]:
@@ -198,18 +307,24 @@ def dict_search_words(feedback,stopwords):
 
 
 # In[6]:
+def data(feedback,stopwords):
+   feedback_dict={}
+   for i in range(0,len(feedback)):
+       
+       feedback_dict[str(i)]=feedback[i]
+
+   return feedback_dict
 
 
-filtered
 
 
 # In[8]:
 
 
-def search():
+def search(string):
     dict_search,index_of_confession=dict_search_words(filtered,stopwords)
-    string = input().lower()
-
+    string = string.lower()
+    
     search_words=dict_search[string[0]]
     search_words=search_words[string[1]]
     length =2
@@ -233,8 +348,16 @@ def search():
 
 
 # In[9]:
+if __name__ == '__main__':
 
-
-search_words,searched_index=search()
-print(search_words,searched_index)
-
+    
+    
+    filtered,profanity,polarity=NLP.filter(feedback)
+    feedback=NLP.data(filtered,stopwords)
+    
+    search_words,searched_index=search("colle")
+    
+    #for confession_index in searched_index:
+     #   print(feedback[str(confession_index)])
+    print(feedback[str(len(feedback)-1)])
+    
